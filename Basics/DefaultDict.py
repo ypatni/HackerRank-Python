9fadb973755327590c270789b1080b4e98273bf9
Yash Patni <ypatni3@gmail.com>
2020-06-27 15:38:10 -0400

    using dictionaries

---
+++ Basics/DefaultDict.py
@@ -0,0 +1,17 @@
+
+from collections import defaultdict
+d = defaultdict(list)
+
+n,m = map(int,input().split())
+
+for i in range(1,n+1):
+    d[input()].append(str(i))
+
+for i in range(m):
+    x = input()
+    if x in d:
+        print(" ".join(d[x]))
+    else:
+        print(-1)
+    
+
