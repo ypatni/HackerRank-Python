1d16a6a415c4d6f304fef3cf374fb768644a08a6
Yash Patni <ypatni3@gmail.com>
2020-07-06 20:42:19 -0400

    using symmetric difference

--- Basics/SetUnions.py
+++ Basics/SetUnions.py
@@ -20,3 +20,12 @@ y = int(input())
 set2 = set(list(map(int, input().split())))
 x = set1.difference(set2)
 print(len(x))
+
+#symmetric_difference
+
+x = int(input())
+set1 = set(list(map(int, input().split())))
+y = int(input())
+set2 = set(list(map(int, input().split())))
+x = set1.symmetric_difference(set2)
+print(len(x))
