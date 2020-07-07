4a8e413639d6dc33420cd9ab296f61aef69aa520
Yash Patni <ypatni3@gmail.com>
2020-07-06 20:39:43 -0400

    using differences

--- Basics/SetUnions.py
+++ Basics/SetUnions.py
@@ -12,3 +12,11 @@ y = int(input())
 set2 = set(list(map(int, input().split())))
 x = set1.intersection(set2)
 print(len(x))
+
+#difference
+x = int(input())
+set1 = set(list(map(int, input().split())))
+y = int(input())
+set2 = set(list(map(int, input().split())))
+x = set1.difference(set2)
+print(len(x))
