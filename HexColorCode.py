1246e1aac3ee758fa882f8f6f5bdd2b5dd24a6c3
Yash Patni <ypatni3@gmail.com>
2020-07-15 20:32:57 -0400

    printing valid hex color codes from an input

---
+++ HexColorCode.py
@@ -0,0 +1,11 @@
+import re
+
+n=int(input())
+
+for i in range(0,n):
+    s=input()
+    x=s.split()
+
+    if len(x)>1  and  '{' not in x:
+        x=re.findall(r'#[a-fA-F0-9]{3,6}',s)
+        [print(i) for  i in x]
